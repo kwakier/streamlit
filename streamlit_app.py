8ee81c279785b25ae5d521ec1d3bdb092278a13e
kwakier <49534600+kwakier@users.noreply.github.com>
2023-11-15 11:36:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,21 @@ import streamlit as st
 import pandas as pd
 import gspread as gs
 import oauth2client as o2c
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
+
+# define the scope
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+# add credentials to the account
+creds = ServiceAccountCredentials.from_json_keyfile_name('hwcollection-afd7a9beea71.json', scope)
+
+# authorize the clientsheet 
+client = gspread.authorize(creds)
+
+# get the instance of the Spreadsheet
+sheet = client.open('Hot Wheels')
+
+# get the first sheet of the Spreadsheet
+sheet_instance = sheet.get_worksheet(0)
+
+sheet_instance.col_count
+
