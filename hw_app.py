1f68c3b70cf8273f749f3d98c7b5fa80582b6288
kwakier <49534600+kwakier@users.noreply.github.com>
2023-12-09 19:33:13 +0100

    Update hw_app.py

--- hw_app.py
+++ hw_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import GSheetsConnection
 url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+conn = st.experimental_connection("gsheets")
 
 data = conn.read(spreadsheet=url, usecols=[0, 1])
 st.dataframe(data)
