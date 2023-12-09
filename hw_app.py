c07b8323c61d47f4ba758ddfa7f5626d9fd35c3e
kwakier <49534600+kwakier@users.noreply.github.com>
2023-12-09 19:31:18 +0100

    Update hw_app.py

--- hw_app.py
+++ hw_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 
 url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
