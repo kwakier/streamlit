f87a0874d023e3bf9dfc0d3ae90f5831c6141152
kwakier <49534600+kwakier@users.noreply.github.com>
2023-12-09 18:48:36 +0100

    Update hw_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,42 @@
+import pandas as pd
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+#st.cache(persist=True, allow_output_mutation=True)
+
+#st.file_uploader(type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
 # Create a connection object.
+url = "https://docs.google.com/spreadsheets/d/1yi769GiV0MsO2hHCJHhQJaIL96_3wu955LZxo79hOkU/edit?pli=1#gid=0"
+
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read( 
+rd = conn.read( 
+    spreadsheet=url,      
     worksheet="Arkusz1",
     ttl="10m",
-    usecols=[0, 1],
-    nrows=8,)
+    usecols=[0]
+    )
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.id} introduced in a :{row.year}:")
+cnt = rd.count()
+st.write(cnt)
+
+#Print results (simple).
+#for row in rd.itertuples():
+#    st.write(f"{row.id}")
+
+#Print results (pretty).
+df = pd.DataFrame(rd, columns = ['id'])
+dfl = df['id'].str.lower()
+
+
+#st.write(dfl)
+st.write(df)
+
+ti = st.text_input(label='szukaj')
+til = ti.lower()
+
+if til is not None :
+    if til in dfl.values: 
+        st.write("Mamy go") 
+    else: 
+        st.write("Kupujemy!")
