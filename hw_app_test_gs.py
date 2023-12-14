eeb16e6c8e7e5f58efd6bcaaf483b076704077ab
kwakier <49534600+kwakier@users.noreply.github.com>
2023-12-14 13:03:34 +0100

    Update hw_app_test_gs.py

--- hw_app_test_gs.py
+++ hw_app_test_gs.py
@@ -2,44 +2,46 @@ import pandas as pd
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+st.camera_input(label, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+
 #pi = st.image('hwlogo.png')
-st.set_page_config(page_title="HW collection", page_icon='hwlogo.png')
+#st.set_page_config(page_title="HW collection", page_icon='hwlogo.png')
 
 #st.cache(persist=True, allow_output_mutation=True)
 
 #st.file_uploader(type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
 # Create a connection object.
-url = "https://docs.google.com/spreadsheets/d/1yi769GiV0MsO2hHCJHhQJaIL96_3wu955LZxo79hOkU/edit?pli=1#gid=0"
+#url = "https://docs.google.com/spreadsheets/d/1yi769GiV0MsO2hHCJHhQJaIL96_3wu955LZxo79hOkU/edit?pli=1#gid=0"
 
-conn = st.connection("gsheets", type=GSheetsConnection)
+#conn = st.connection("gsheets", type=GSheetsConnection)
 
-rd = conn.read( 
-    spreadsheet=url,      
-    worksheet="Arkusz1",
-    ttl="10m",
-    usecols=[0]
-    )
+#rd = conn.read( 
+#    spreadsheet=url,      
+#    worksheet="Arkusz1",
+#    ttl="10m",
+#    usecols=[0]
+#    )
 
-cnt = rd.count()
-st.write(cnt)
+#cnt = rd.count()
+#st.write(cnt)
 
 #Print results (simple).
 #for row in rd.itertuples():
 #    st.write(f"{row.id}")
 
 #Print results (pretty).
-df = pd.DataFrame(rd, columns = ['id'])
-dfl = df['id'].str.lower()
+#df = pd.DataFrame(rd, columns = ['id'])
+#dfl = df['id'].str.lower()
 
-ti = st.text_input(label='szukaj')
-til = ti.lower()
+#ti = st.text_input(label='szukaj')
+#til = ti.lower()
 
-if til is not None :
-    if til in dfl.values: 
-        st.write("Mamy go") 
-    else: 
-        st.write("Kupujemy!")
+#if til is not None :
+#    if til in dfl.values: 
+#        st.write("Mamy go") 
+#    else: 
+#        st.write("Kupujemy!")
 
 #st.write(dfl)
-st.write(df)
+#st.write(df)
