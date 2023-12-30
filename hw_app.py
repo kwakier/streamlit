a20560823c81cf756798ec749bb26ea79991b4de
kwakier <49534600+kwakier@users.noreply.github.com>
2023-12-30 14:47:36 +0100

    Update hw_app.py

--- hw_app.py
+++ hw_app.py
@@ -49,7 +49,7 @@ if ti:
         ro = st.text_input(label='rocznik')
         options = set(df['marka'])
         ma = st.selectbox('marka',options)
-        if ma is none:
+        if ma == "":
             dodmar = st.text_input("Nowa marka")
         else:
             dodmar = ma
