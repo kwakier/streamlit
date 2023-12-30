2e8dcd6f70155bedacf2c36b7ee4dacc922e84e2
kwakier <49534600+kwakier@users.noreply.github.com>
2023-12-30 14:12:31 +0100

    Update hw_app.py

--- hw_app.py
+++ hw_app.py
@@ -48,10 +48,12 @@ if ti:
     else:
         st.write("Kupujemy!")
         ro = st.text_input(label='rocznik')
+        ma = st.selectbox(label='marka',set(df['marka']))
         dodaj_hw = st.button("Dodaj")
         if dodaj_hw:
             sh.sheet1.update_cell(cnt()+2,1, ti)
             sh.sheet1.update_cell(cnt()+2,2, ro)
+            sh.sheet1.update_cell(cnt()+2,4, ma)
            
 else:
     st.write("Sprawdźmy")
