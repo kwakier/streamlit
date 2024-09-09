8c2550daf7b3296b79edc7d19a1a7b23c78c9a22
kwakier <49534600+kwakier@users.noreply.github.com>
2024-09-09 14:25:52 +0200

    Update wydatki.py

--- wydatki/wydatki.py
+++ wydatki/wydatki.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("2_dodaj.py"), st.Page("1_dashboard.py")])
-pg.run()
-
-import streamlit as st
-
 create_page = st.Page("2_dodaj.py", title="Dodaj wpis", icon=":material/add_circle:")
 delete_page = st.Page("1_dashboard.py", title="Dashboard", icon=":material/delete:")
 
