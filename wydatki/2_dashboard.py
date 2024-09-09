965425158e8e6a76823d44355fc4de61c768c07a
kwakier <49534600+kwakier@users.noreply.github.com>
2024-09-09 22:20:24 +0200

    Update 2_dashboard.py

--- wydatki/2_dashboard.py
+++ wydatki/2_dashboard.py
@@ -24,11 +24,10 @@ sh = gc.open_by_key('1VLmAF5CrcEmMMAuDLN4z4YQ2XS4YYtBKMIVDKgtp1v4')
 
 rd = sh.sheet1.get_all_records()
 
-df = pd.DataFrame(rd, columns = ['id','Kategoria','Kwota','Data','Uwagi'])
+df = pd.DataFrame(rd, columns = ['id','Kategoria','Kwota','Data','Uwagi'],hide_index = True)
 
 df['Data'] = pd.to_datetime(df['Data'])
 
 monthly_sales = df.groupby([df['Data'].dt.month, 'Kategoria'])['Kwota'].sum().reset_index()
 
 monthly_sales
-df['Data'].dt.month
