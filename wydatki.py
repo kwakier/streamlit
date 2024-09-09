f6a127d2d53aee571a44a5a1c3bd7938898edb91
kwakier <49534600+kwakier@users.noreply.github.com>
2024-09-09 13:09:19 +0200

    Update wydatki.py

--- wydatki.py
+++ wydatki.py
@@ -33,7 +33,7 @@ cnt()
 options = set(df['Kategoria'])
 kat = st.selectbox("Kategoria",options)
 kwo = st.number_input(label='Kwota')
-dat = st.text_input(label='Data'),
+#da = st.text_input(label='Data'),
 #dat = st.date_input(label='Data',format="YYYY-MM-DD"),
 uwa = st.text_input(label='Uwagi')
 if kat == "Dodaj":
@@ -45,6 +45,6 @@ if dodaj:
     sh.sheet1.update_cell(cnt()+2,1, cnt()+1)
     sh.sheet1.update_cell(cnt()+2,2, dodkat)
     sh.sheet1.update_cell(cnt()+2,3, kwo)
-    sh.sheet1.update_cell(cnt()+2,4, dat)
+    sh.sheet1.update_cell(cnt()+2,4, now())
     sh.sheet1.update_cell(cnt()+2,5, uwa)
 df
