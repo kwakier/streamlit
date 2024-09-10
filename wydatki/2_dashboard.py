b261f1ee1bb227943ec6f82fd79fdae6bd1b576e
kwakier <49534600+kwakier@users.noreply.github.com>
2024-09-10 22:17:36 +0200

    Update 2_dashboard.py

--- wydatki/2_dashboard.py
+++ wydatki/2_dashboard.py
@@ -3,7 +3,7 @@ import gspread
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pyplot as plt
+import matplotlib.pyplot as plt
 
 credentials = {
     "type": "service_account",
