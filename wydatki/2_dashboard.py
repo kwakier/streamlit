46c5b16b07dbc0eb85a4066343e1da4ca6271222
kwakier <49534600+kwakier@users.noreply.github.com>
2024-09-10 22:52:59 +0200

    Update 2_dashboard.py

--- wydatki/2_dashboard.py
+++ wydatki/2_dashboard.py
@@ -43,4 +43,4 @@ mth = st.multiselect("Month",options_month)
 year = st.multiselect("Year",options_year)
 
 # Create the bar chart
-st.bar_chart(df_chart['Kwota'], df_chart['Kategoria'])
+st.bar_chart(df_chart['Kwota'], x=df_chart['Kategoria'])
