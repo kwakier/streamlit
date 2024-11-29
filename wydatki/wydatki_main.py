f3e234617a51ca9619cd3ac7eee2bfd77d801737
kwakier <49534600+kwakier@users.noreply.github.com>
2024-11-29 15:14:20 +0100

    Update wydatki_main.py

--- wydatki/wydatki_main.py
+++ wydatki/wydatki_main.py
@@ -3,6 +3,7 @@ import streamlit as st
 dodaj = st.Page("1_dodaj_wpis.py", title="Dodaj wpis", icon=":material/add_circle:")
 dashboard = st.Page("2_dashboard.py", title="Dashboard", icon=":material/insert_chart_outlined:")
 hwapp = st.Page("3_hw_app.py", title="Hot Wheels", icon=":material/directions_car:")
+hwdashboard = st.Page("4_hw_dashboard.py", title="HW Dashboard", icon=":material/directions_car:")
 
 pg = st.navigation([dodaj, dashboard, hwapp])
 st.set_page_config(page_title="Moje appki", page_icon=":material/edit:")
